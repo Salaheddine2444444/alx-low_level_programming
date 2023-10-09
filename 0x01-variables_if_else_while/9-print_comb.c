d2d18c6b530f1d87a53e27a4a1b6f9c552c4e860
Salaheddine2444444 <salahmallouk44@gmail.com>
2023-10-09 11:46:50 -0700

    bb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar('0' + num[x]);
+		if (x <= 8)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
