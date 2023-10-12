d4c7bba9f50f1ababbb0b9bd75b19a3571915d00
Salaheddine2444444 <salahmallouk44@gmail.com>
2023-10-12 11:11:59 -0700

    c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - prnit the multiplies of tow intiger
+ * @a: the first input
+ * @b: the second input
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
