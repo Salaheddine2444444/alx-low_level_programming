0031c70bdb59bc108cc3f35543f2f631940447d6
Salaheddine2444444 <salahmallouk44@gmail.com>
2023-10-25 15:09:39 -0700

    bbbb

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - count the factorial of a number
+ * @n: the input
+ * Return: n or -1
+ * owned by: SmoxHakim
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
