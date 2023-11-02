7ff1b1fecbb36b74586d151cd772fb429cd7cb9f
Salaheddine2444444 <salahmallouk44@gmail.com>
2023-11-02 04:22:04 -0700

    ggg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start of the progrm
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: 0
+ * owned by: Sinco
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int res = 0;
+
+	if (argc == 1)
+	{
+	puts("0");
+	return (0);
+	}
+
+	for (x = 1; x < argc; x++)
+	{
+
+		if (atoi(argv[x]) == 0 && argv[x][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+
+	res += atoi(argv[x]);
+
+	}
+	printf("%d\n", res);
+	return (0);
+}
