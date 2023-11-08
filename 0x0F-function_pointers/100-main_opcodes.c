c2ba7d197fd0e0e0e68d5caf1d19348d8029357e
Salaheddine2444444 <salahmallouk44@gmail.com>
2023-11-08 15:09:45 -0800

    babb

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,58 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * print_opcodes - print the opcodes of the main function
+ * @bytes: number of bytes to print
+ * owned by: Sinco
+ */
+
+void print_opcodes(int bytes)
+{
+	unsigned char *main_ptr = (unsigned char *)print_opcodes;
+	int x;
+
+	for (x = 0; x < bytes; x++)
+	{
+		printf("%02x", main_ptr[x]);
+		if (x < bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+		printf("\n");
+}
+
+/**
+ * main - start of the program
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 or 1 or 2
+ * owned by: Sinco
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(bytes);
+
+	return (0);
+}
