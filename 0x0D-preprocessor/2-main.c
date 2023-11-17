cf32959f509f15df0048c253e7e7fb0a4aa61732
Salaheddine2444444 <salahmallouk44@gmail.com>
2023-11-17 07:49:17 -0800

    rbrb

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
+ * main - start of the program
+ * Return: 0
+ * owned by: SmoxHskim
  */
-int ain(void)
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
