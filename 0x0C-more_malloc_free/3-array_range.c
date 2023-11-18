79b217234f27dcafc1e8f5732e815e0004c66505
Salaheddine2444444 <salahmallouk44@gmail.com>
2023-11-18 09:42:41 -0800

    ttt

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_range - creates an array
+ * @min: first input
+ * @max: second input
+ * Return: pointer
+ * owned by: Sinco
+ */
+
+int *array_range(int min, int max)
+{
+
+	unsigned int total, x;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+
+	total = (max - min + 1);
+
+	ptr = (int *)malloc(sizeof(int) * total);
+
+	if (!ptr)
+		return (NULL);
+
+	for (x = 0; x < total; x++)
+	{
+		ptr[x] = min;
+		min++;
+	}
+
+
+	return (ptr);
+}
